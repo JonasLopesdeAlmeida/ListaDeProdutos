3a039937410198ec3f6010bc8ae96d43c6c150e9
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-04-13 23:14:38 -0300

    React-Transiction-Group Disponivel

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 
-import {Channel} from '../services/EventService';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import { Channel } from '../services/EventService';
 
 class ProductList extends Component {
     static defaultProps = {
         products: []
     }
 
-  remove(product){
+    remove(product) {
 
-    Channel.emit('product:remove', product.id)
-  }
+        Channel.emit('product:remove', product.id)
+    }
 
     render() {
 
         const { props } = this;
         return (
             <ul className="product-list">
-                {
-                    props.products.map(product => (
+                <TransitionGroup>
 
-                        <li key={product.id} className="product-list-item">
-                            <button onClick={this.remove.bind(this, product)}>X</button>
-                            <img src={product.image} alt={product.description}/>
-                            <div>{product.description}</div>
-                            <div>{product.price}</div>
+                    {
+                        props.products.map(product => (
+                            <CSSTransition key={product.id} timeout={300} classNames="product">
+                                <li  className="product-list-item">
+                                    <button onClick={this.remove.bind(this, product)}>X</button>
+                                    <img src={product.image} alt={product.description} />
+                                    <div>{product.description}</div>
+                                    <div>{product.price}</div>
 
-                        </li>
+                                </li>
 
-                    ))
+                            </CSSTransition>
 
-                }
+
+                        ))
+
+                    }
+                </TransitionGroup>
+
 
             </ul>
         )
