9f9ef77b23331a39e04080eae6ccaeba9a81e3b2
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-04-14 17:32:21 -0300

    Rota entre os componentes Disponível.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import NewProductView from './views/NewProductView';
 import ProductListView from './views/ProductListView';
 
-function App() {
+class App extends Component {
+  render(){ 
   return (
     <div className="App">
-    <NewProductView/>
-    <ProductListView/>
+      <Router>
+        <div>
+          <header>
+          <ul className="link-list">
+           <li>
+             <Link to={'/'}>Novo</Link>
+           </li>
+           <li>
+             <Link to={'/list'}>Lista</Link>
+           </li>
+          </ul>
+          </header>
+          <div>
+            <Route path={'/'} exact component={NewProductView}/>
+            <Route path={'/list'} component={ProductListView}/>
+          </div>
+        </div>
+      </Router>
     </div>
   );
+}
 }
 
 export default App;
